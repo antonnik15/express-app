dc3e7134d89b2e4ed119ac90e62c9048db02f4c6
antonnik15 <nikiforovkuvv@mail.ru>
2022-11-09 14:40:52 +0300

    init commit

--- src/repositories/blogs-repositories/blogs-query-repository.ts
+++ src/repositories/blogs-repositories/blogs-query-repository.ts
@@ -5,24 +5,24 @@ import {ObjectId} from "mongodb";
 export const blogsQueryRepository = {
     async findAllBlogs(): Promise<outputBlogType[]> {
         const dbBlogs: DbBlogType[] = await blogsCollection.find({}).toArray()
-        dbBlogs.map(blog => {
-            mapDbBlogTypeToOutputBlogType(blog)
+        return dbBlogs.map((blog: DbBlogType) => {
+            return this.mapDbBlogTypeToOutputBlogType(blog)
         })
-        return dbBlogs;
     },
     async findBlogById(id: string): Promise<outputBlogType | null> {
         const blogById: DbBlogType | null = await blogsCollection.findOne({id: id})
-        return mapDbBlogTypeToOutputBlogType(blogById!)
+        return this.mapDbBlogTypeToOutputBlogType(blogById!)
+    },
+    mapDbBlogTypeToOutputBlogType(dbBlog: DbBlogType): outputBlogType {
+        return {
+            id: dbBlog.id,
+            name: dbBlog.name,
+            youtubeUrl: dbBlog.youtubeUrl,
+            createdAt: dbBlog.createdAt
+        }
     }
 }
-const mapDbBlogTypeToOutputBlogType = (dbBlog: DbBlogType): outputBlogType => {
-    return {
-        id: dbBlog.id,
-        name: dbBlog.name,
-        youtubeUrl: dbBlog.youtubeUrl,
-        createdAt: dbBlog.createdAt
-    }
-}
+
 type outputBlogType ={
     id: string
     name: string
--- src/repositories/posts-repositories/posts-query-repository.ts
+++ src/repositories/posts-repositories/posts-query-repository.ts
@@ -4,27 +4,27 @@ import {ObjectId} from "mongodb";
 export const postsQueryRepository = {
     async findAllPosts(): Promise<OutPutPostType[]> {
         const dbPosts: DbPostType[] = await postsCollection.find({}).toArray()
-        dbPosts.map(post => {
-            return mapDbPostToOutPutPostType(post)
-        })
-        return dbPosts;
+        return dbPosts.map((post) => {
+            return this.mapDbPostToOutPutPostType(post)
+        });
     },
     async findPostById(id: string): Promise<OutPutPostType | null> {
         const dbPostById: DbPostType | null =  await postsCollection.findOne({id: id})
-        return mapDbPostToOutPutPostType(dbPostById!)
+        return this.mapDbPostToOutPutPostType(dbPostById!)
     },
-}
-const mapDbPostToOutPutPostType = (dbPost: DbPostType) => {
-    return {
-        id: dbPost!.id,
-        title: dbPost!.title,
-        shortDescription: dbPost!.shortDescription,
-        content: dbPost!.content,
-        blogId: dbPost!.blogId,
-        blogName: dbPost!.blogName,
-        createdAt: dbPost!.createdAt
+    mapDbPostToOutPutPostType(dbPost: DbPostType) {
+        return {
+            id: dbPost!.id,
+            title: dbPost!.title,
+            shortDescription: dbPost!.shortDescription,
+            content: dbPost!.content,
+            blogId: dbPost!.blogId,
+            blogName: dbPost!.blogName,
+            createdAt: dbPost!.createdAt
+        }
     }
 }
+
 type DbPostType = {
     _id: ObjectId
     id: string
