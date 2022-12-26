34e59c2abc8fc92558fb3840d7d4cea05a190dae
antonnik15 <nikiforovkuvv@mail.ru>
2022-12-26 19:11:52 +0300

    init commit

--- src/routes/delete-all-data.ts
+++ src/routes/delete-all-data.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 export const deleteRouter = Router({})
 
-deleteRouter.delete("/", async (req: Request, res: Response) => {
-    await mongoose.connection.db.dropDatabase()
+deleteRouter.delete("/",  (req: Request, res: Response) => {
+    mongoose.connection.db.dropDatabase()
     res.sendStatus(204)
 })
